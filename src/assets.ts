2118df855143b2ba0c7c66b81ca9110d686e1465
Birdman1104 <arsenmazmanyan11@gmail.com>
2024-08-07 03:25:51 +0400

    Added assets.

--- src/assets.ts
+++ src/assets.ts
@@ -2,8 +2,25 @@
 
 export const assets = {
     images: {
+        'game/alley_fountain.png': require('../assets/images/game/alley_fountain.png').default,
         'game/bkg.jpg': require('../assets/images/game/bkg.jpg').default,
+        'game/buy_cafe.png': require('../assets/images/game/buy_cafe.png').default,
+        'game/buy_hospital.png': require('../assets/images/game/buy_hospital.png').default,
+        'game/buy_house.png': require('../assets/images/game/buy_house.png').default,
+        'game/buy_park.png': require('../assets/images/game/buy_park.png').default,
+        'game/cafe.png': require('../assets/images/game/cafe.png').default,
+        'game/coin_bar.png': require('../assets/images/game/coin_bar.png').default,
+        'game/fail.png': require('../assets/images/game/fail.png').default,
+        'game/food_bar.png': require('../assets/images/game/food_bar.png').default,
+        'game/fortune_wheel.png': require('../assets/images/game/fortune_wheel.png').default,
         'game/hand.png': require('../assets/images/game/hand.png').default,
+        'game/health_bar.png': require('../assets/images/game/health_bar.png').default,
+        'game/hospital.png': require('../assets/images/game/hospital.png').default,
+        'game/house.png': require('../assets/images/game/house.png').default,
+        'game/logo.png': require('../assets/images/game/logo.png').default,
+        'game/progress_bar.png': require('../assets/images/game/progress_bar.png').default,
+        'game/well_bar.png': require('../assets/images/game/well_bar.png').default,
+        'game/winter_fountain.png': require('../assets/images/game/winter_fountain.png').default,
     },
     particles: {},
     sounds: {},
@@ -11,7 +28,27 @@ export const assets = {
     atlases: {},
 };
 
-export const Images = { 'game/bkg': 'game/bkg.jpg', 'game/hand': 'game/hand.png' };
+export const Images = {
+    'game/alley_fountain': 'game/alley_fountain.png',
+    'game/bkg': 'game/bkg.jpg',
+    'game/buy_cafe': 'game/buy_cafe.png',
+    'game/buy_hospital': 'game/buy_hospital.png',
+    'game/buy_house': 'game/buy_house.png',
+    'game/buy_park': 'game/buy_park.png',
+    'game/cafe': 'game/cafe.png',
+    'game/coin_bar': 'game/coin_bar.png',
+    'game/fail': 'game/fail.png',
+    'game/food_bar': 'game/food_bar.png',
+    'game/fortune_wheel': 'game/fortune_wheel.png',
+    'game/hand': 'game/hand.png',
+    'game/health_bar': 'game/health_bar.png',
+    'game/hospital': 'game/hospital.png',
+    'game/house': 'game/house.png',
+    'game/logo': 'game/logo.png',
+    'game/progress_bar': 'game/progress_bar.png',
+    'game/well_bar': 'game/well_bar.png',
+    'game/winter_fountain': 'game/winter_fountain.png',
+};
 
 export const Sounds = {};
 
